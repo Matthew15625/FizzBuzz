c13d7256db9f93f1c29ef569d775b74485a56935
Matthew Goodship <matthewwgoodship@icloud.com>
2021-01-08 13:23:04 +0000

    Coded python implementation

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ For i <- 1 To 15
     #python version of if statement above# output = output if output else str(i)
     print(output)
 '''
+
+
+keyNumbers = {3:"Fizz", 5:"Buzz"}
+for i in range(1, 16):
+    output = ""
+    for num in list(keyNumbers.keys()):
+        if i % num == 0:
+            output += keyNumbers[num]
+    output = output if output else str(i)
+
+    print(output)
