c60b3941fe1b2d52cadc89e7064b273e4c1905ee
Matthew Goodship <matthewwgoodship@icloud.com>
2021-01-17 16:54:13 +0000

    User now inputs how far the program should count

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 '''
 keyNumbers = [3, 5]
 keyWords = ["Fizz", "Buzz"]
-FOR i <- 1 TO 15
+OUTPUT "How far to count?"
+INPUT howFar
+WHILE howFar < 1
+    OUTPUT "Not a valid number, please try again."
+    INPUT howFar
+ENDWHILE
+FOR i <- 1 TO howFar
     output <- ""
     FOR j <- 1 To 2
-        IF i MOD keyNumbers[j] = 0 THEN
+        IF i MOD keyNumbers[j] = 0
             output <- output + keyWords[j]
-    IF output = "" THEN
-        output = str(i)
-    print(output)
+        ENDIF
+    ENDFOR
+    IF output = ""
+        output <- str(i)
+    ENDIF
+    OUTPUT output
+ENDFOR
 '''
 
 
 keyNumbers = [3, 5]
 keyWords = ["Fizz", "Buzz"]
-for i in range(1, 16):
+howFar = ""
+validInput = False
+while not validInput:
+    howFar = input("How far to count?")
+    try:
+        howFar = int(howFar)
+        if howFar >= 1:
+            validInput = True
+            break
+    except:
+        pass
+    print("Not a valid number, please try again.")
+
+for i in range(1, howFar+1):
     output = ""
     for j in range(0, len(keyNumbers)):
         if i % keyNumbers[j] == 0:
-            output += keyWords[num]
+            output += keyWords[j]
     if output == "":
         output = i
 
